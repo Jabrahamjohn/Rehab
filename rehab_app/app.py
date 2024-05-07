0d3587ea36ff179c7b54c615e7084db63bfd3402
Abraham John <jabrahamjohns@gmail.com>
2024-05-07 11:09:13 +0300

    routes defined

--- rehab_app/app.py
+++ rehab_app/app.py
@@ -13,6 +13,17 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/services')
+def contact():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/resources')
+def contact():
+    return render_template('resources.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
