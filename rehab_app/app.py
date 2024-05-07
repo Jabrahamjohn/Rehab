0eb4d8e94b4dd975f42eea96303a9fbb67efc609
Abraham John <jabrahamjohns@gmail.com>
2024-05-07 12:18:39 +0300

    lets see this

--- rehab_app/app.py
+++ rehab_app/app.py
@@ -5,25 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Render the index.html template
+    """Renders the index.html template with a title."""
     title = 'Rehab Center Management System'
     return render_template('index.html', title=title)
 
 @app.route('/about')
 def about():
+    """Renders the about.html template."""
     return render_template('about.html')
 
 @app.route('/services')
-def contact():
+def services():
+    """Renders the services.html template."""
     return render_template('services.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/resources')
-def contact():
-    return render_template('resources.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
